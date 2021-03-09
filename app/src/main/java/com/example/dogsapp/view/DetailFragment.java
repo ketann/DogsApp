fb08abb81d3d7b91859c49a0cd3cd83122266448
Ketan Nakum <dhaval@lionobytes.com>
2021-03-10 03:50:10 +0530

    Clean up the code for next functionality

--- app/src/main/java/com/example/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsapp/view/DetailFragment.java
@@ -4,26 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavDirections;
-import androidx.navigation.Navigation;
-
 import com.example.dogsapp.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetailFragment extends Fragment {
 
-    @BindView(R.id.floatingActionButton2)
-    FloatingActionButton fabDetail;
-    @BindView(R.id.textView2)
-    TextView textView;
 
     private int dogUuid;
 
@@ -45,16 +33,6 @@ public class DetailFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         if (getArguments() != null) {
             dogUuid = DetailFragmentArgs.fromBundle(getArguments()).getDogUuid();
-            textView.setText(String.valueOf(dogUuid));
         }
-        fabDetail.setOnClickListener(v -> {
-            onGotoList();
-        });
-    }
-
-    void onGotoList() {
-        NavDirections actionList = DetailFragmentDirections.actionList();
-        Navigation.findNavController(fabDetail).navigate(actionList);
-
     }
 }
--- app/src/main/java/com/example/dogsapp/view/ListFragment.java
+++ app/src/main/java/com/example/dogsapp/view/ListFragment.java
@@ -5,22 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
-
 import com.example.dogsapp.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class ListFragment extends Fragment {
 
-    @BindView(R.id.floatingActionButton)
-    FloatingActionButton fab;
 
     public ListFragment() {
         // Required empty public constructor
@@ -33,19 +24,5 @@ public class ListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
         ButterKnife.bind(this, view);
         return view;
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        fab.setOnClickListener(v -> {
-            onGotoDetails();
-        });
-    }
-
-    void onGotoDetails() {
-        ListFragmentDirections.ActionDetail action = ListFragmentDirections.actionDetail();
-        action.setDogUuid(7);
-        Navigation.findNavController(fab).navigate(action);
     }
 }
