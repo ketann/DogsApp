af419780bff09edca0e7db96668aec99fc64c662
Ketan Nakum <dhaval@lionobytes.com>
2021-03-10 03:14:53 +0530

    Implement Navigation in DetailFragment

--- app/src/main/java/com/example/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsapp/view/DetailFragment.java
@@ -2,13 +2,27 @@ package com.example.dogsapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import com.example.dogsapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailFragment extends Fragment {
+
+    @BindView(R.id.floatingActionButton2)
+    FloatingActionButton fabDetail;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -18,6 +32,22 @@ public class DetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        fabDetail.setOnClickListener(v -> {
+            onGotoList();
+        });
+    }
+
+    void onGotoList() {
+        NavDirections actionList = DetailFragmentDirections.actionList();
+        Navigation.findNavController(fabDetail).navigate(actionList);
+
     }
 }
