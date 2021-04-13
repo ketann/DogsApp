e9ea898769790a0dc95befb605a94bd65cdd9cf4
Ketan Nakum <dhaval@lionobytes.com>
2021-04-14 00:39:10 +0530

    Implement 1) Permission and Sharing dialog

--- app/src/main/java/com/example/dogsapp/view/DetailFragment.java
+++ app/src/main/java/com/example/dogsapp/view/DetailFragment.java
@@ -1,16 +1,24 @@
 package com.example.dogsapp.view;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -23,8 +31,10 @@ import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
 import com.example.dogsapp.R;
 import com.example.dogsapp.databinding.FragmentDetailBinding;
+import com.example.dogsapp.databinding.SendSmsDialogBinding;
 import com.example.dogsapp.model.DogBreed;
 import com.example.dogsapp.model.DogPalette;
+import com.example.dogsapp.model.SmsInfo;
 import com.example.dogsapp.util.Util;
 import com.example.dogsapp.viewmodel.DetailViewModel;
 
@@ -36,6 +46,8 @@ public class DetailFragment extends Fragment {
     private int dogUuid;
     private DetailViewModel viewModel;
     private FragmentDetailBinding binding;
+    private Boolean sendSmsStarted = false;
+    private DogBreed currentDog;
 
     public DetailFragment() {
         // Required empty public constructor
@@ -47,6 +59,7 @@ public class DetailFragment extends Fragment {
         // Inflate the layout for this fragment
         FragmentDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false);
         this.binding = binding;
+        setHasOptionsMenu(true);
         return binding.getRoot();
     }
 
@@ -66,6 +79,7 @@ public class DetailFragment extends Fragment {
     private void observeViewModel() {
         viewModel.dogLiveData.observe(this, dogBreed -> {
             if (dogBreed != null && dogBreed instanceof DogBreed && getContext() != null) {
+                currentDog = dogBreed;
                 binding.setDog(dogBreed);
                 if (dogBreed.imageUrl != null) {
                     setBackgroundColor(dogBreed.imageUrl);
@@ -95,4 +109,74 @@ public class DetailFragment extends Fragment {
                     }
                 });
     }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.detail_menu, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_send_sms: {
+                if (!sendSmsStarted) {
+                    sendSmsStarted = true;
+                    ((MainActivity) getActivity()).checkSmsPermission();
+                }
+                break;
+            }
+            case R.id.action_share: {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Check out this application");
+                intent.putExtra(Intent.EXTRA_TEXT, currentDog.dogBreed + " bred for " + currentDog.bredFor);
+                intent.putExtra(Intent.EXTRA_STREAM, currentDog.imageUrl);
+                startActivity(Intent.createChooser(intent, "Share with"));
+                break;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onPermissionResult(Boolean permissionGranted) {
+        if (isAdded() && sendSmsStarted && permissionGranted) {
+            SmsInfo smsInfo = new SmsInfo("", currentDog.dogBreed + " bred for " + currentDog.bredFor, currentDog.imageUrl);
+
+            SendSmsDialogBinding dialogBinding = DataBindingUtil.inflate(
+                    LayoutInflater.from(getContext()),
+                    R.layout.send_sms_dialog,
+                    null,
+                    false
+            );
+
+            new AlertDialog.Builder(getContext())
+                    .setView(dialogBinding.getRoot())
+                    .setPositiveButton("Send SMS", ((dialog, which) -> {
+                        if (dialogBinding.smsDestination.getText().toString().isEmpty()) {
+                            smsInfo.to = dialogBinding.smsDestination.getText().toString();
+                            sendSMS(smsInfo);
+                        }
+                    }))
+                    .setNegativeButton("Cancel", ((dialog, which) -> {
+                    }))
+                    .show();
+
+            sendSmsStarted = false;
+
+            dialogBinding.setSmsInfo(smsInfo);
+        }
+    }
+
+    /*
+     * send sms via sim card carrier
+     * */
+    private void sendSMS(SmsInfo smsInfo) {
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, 0);
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(smsInfo.to, null, smsInfo.text, pendingIntent, null);
+    }
+
 }
